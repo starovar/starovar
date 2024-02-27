922be1925be7fa2141eb24b0d73eede1e75f811f
Serhii <76422002+gitsolaicity@users.noreply.github.com>
2024-02-27 13:34:41 +0200

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 // export const runtime = 'edge';
-export const config = {
-  runtime: 'edge',
-}
+export const runtime = 'edge';
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
